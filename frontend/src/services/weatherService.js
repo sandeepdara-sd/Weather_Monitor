963bb5cd8ee8842a16996c2a7aae57a53f34515b
Sandeep Dara <148864994+sandeepdara-sd@users.noreply.github.com>
2025-02-02 20:44:46 +0530

    Update weatherService.js

--- frontend/src/services/weatherService.js
+++ frontend/src/services/weatherService.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchWeatherData = async () => {
-    const response = await axios.get('http://localhost:5000/api/weather/current');
+    
+    const response = await axios.get(`${API_URL}/api/weather/current`);
     return response.data;
 };
 
 export const fetchDailySummary = async () => {
-    const response = await axios.get('http://localhost:5000/api/weather/summary');
+    const response = await axios.get(`${API_URL}/api/weather/summary`);
     return response.data;
 };
+
